4c0210febb4d85989768236bed09cf8b0aa26eba
YashSrivastava7008 <yashsrivastava7008@gmail.com>
2026-08-16 18:35:41 +0530

    chore: clean up diagnostic scripts

--- test.py
+++
@@ -1,5 +0,0 @@
-import re
-html = open('live-site.html', encoding='utf-16').read()
-matches = re.findall(r'.{0,40}\.jpg|.{0,40}\.png|.{0,40}\.jpeg', html)
-for match in set(matches):
-    print(match.strip())
